b897a846d081237bf01c50d3388a16e21612b26c
Michael Andrianaivomanana <nandrianaivomanana@gmail.com>
2020-06-16 21:56:05 +0300

    Upload first

--- application/config/database.php
+++ application/config/database.php
@@ -74,10 +74,10 @@ $active_group = 'default';
 $query_builder = TRUE;
 
 $db['default'] = array(
-	'dsn'	=> 'pgsql:host=localhost;port=5432;dbname=lhrn2_monolitique',
+	'dsn'	=> 'pgsql:host=localhost;port=5432;dbname=loharano_v2',
 	'hostname' => '',
 	'username' => 'postgres',
-	'password' => '987456321',
+	'password' => '123456',
 	'database' => '',
 	'dbdriver' => 'pdo',
 	'dbprefix' => '',
