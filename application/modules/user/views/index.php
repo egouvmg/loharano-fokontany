dacc52eaacbec7f6b8e501bf831ff5f1d6cd5f05
Michael Andrianaivomanana <nandrianaivomanana@gmail.com>
2020-07-01 18:43:23 +0300

    Indicateurs démographiques

--- application/modules/user/views/index.php
+++ application/modules/user/views/index.php
@@ -114,10 +114,10 @@
                     <h6>Indicateurs démographiques</h6>
                     <div class="container-bloc-link">
                       <a href="#" class="bloc-link color-6" data-toggle="tooltip" data-placement="bottom" data-html="true" title="<ul class='m-0'><li>Moyenne d'age des hommes : <strong><?=$male_avg_age;?></strong></li><li>Moyenne d'age des femmes : <strong><?=$female_avg_age;?></strong></li></ul>">
-                        <span class="pourcent"><?= $female_ratio;?>% / <?= $male_ratio;?>%</span> <?=$this->lang->line('count_citizen');?> Hommes/Femmes
+                        <span class="pourcent"><?= $male_ratio;?>% / <?= $female_ratio;?>%</span> <?=$this->lang->line('count_citizen');?> Hommes/Femmes
                       </a>
                       <a href="#" class="bloc-link color-6" data-toggle="tooltip" data-placement="bottom" data-html="true" title="<ul class='m-0'><li>Nombre de mineurs masculins : <strong><?=$minor_male;?></strong></li><li>Nombre de mineurs féminins : <strong><?=$minor_female;?></strong></li><li>Nombre de majeurs masculins : <strong><?=$major_male;?></strong></li><li>Nombre de majeurs féminins : <strong><?=$major_female;?></strong></li></ul>">
-                        <span class="pourcent"><?= $minor_ratio;?>% / <?= $major_ratio;?>%</span> <?=$this->lang->line('count_household');?> Majeurs/Mineurs
+                        <span class="pourcent"><?= $minor_ratio;?>% / <?= $major_ratio;?>%</span> <?=$this->lang->line('count_household');?> Mineurs/Majeurs
                       </a>
                     </div>
                   </div>
